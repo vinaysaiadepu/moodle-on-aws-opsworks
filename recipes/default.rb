fa5eeecca9dc4f9c4a3c8a7a1aa890470ed239f8
jamesoflol <james.lol@hale.com.au>
2016-05-24 15:39:33 +1000

    Git clone on app deploy

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,9 @@
 Chef::Log.info("********* HELLO WORLD! *************")
+
+Chef::Log.info("********* Cloning app from github *************")
+app = search(:aws_opsworks_app).first
+app_path = "/srv/#{app['shortname']}"
+git app_path do
+	repository app["app_source"]["url"]
+	revision app["app_source"]["revision"]
+end
