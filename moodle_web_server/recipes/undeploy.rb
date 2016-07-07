ae2f64659ee410c9721786906ad05eddb75a909a
Shyam Saraswati <shyam.saraswati@gmail.com>
2016-07-08 09:53:12 +1000

    created undeploy for web server

---
+++ moodle_web_server/recipes/undeploy.rb
@@ -0,0 +1,26 @@
+# Cloning app from github - this will only grab the first app and ignore all others. This first/only app should be a Moodle github repo
+app = search(:aws_opsworks_app).first
+app_path = "/srv/#{app['shortname']}"
+
+directory app_path do
+	recursive true
+	action :delete
+end
+
+# Symlink app to /var/www/html
+directory '/var/www/html' do
+	action :delete
+	ignore_failure true
+end
+
+link '/var/www/html' do
+	to app_path
+end
+
+file "/tmp/#{app['shortname']}" + ".zip" do
+	action :delete
+end
+
+
+
+
