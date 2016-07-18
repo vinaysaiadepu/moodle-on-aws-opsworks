b417aa4247d2a6e7092bb1938e618445d8ae6147
Shyam Saraswati <shyam.saraswati@gmail.com>
2016-07-18 14:00:54 +1000

    Seperate memcached instance

--- cookbooks/moodle_opsworks/recipes/setup.rb
+++ cookbooks/moodle_opsworks/recipes/setup.rb
@@ -1,2 +1,13 @@
 include_recipe 'moodle_opsworks::apache'
-include_recipe 'memcached'
+
+memcached_instance 'memcached_sessions' do
+  port 11_212
+  memory 64
+  action :start
+end
+## Seperate cache so we can purge without killing session
+memcached_instance 'memcached_application' do
+  port 111_212
+  memory 128
+  action :start
+end
