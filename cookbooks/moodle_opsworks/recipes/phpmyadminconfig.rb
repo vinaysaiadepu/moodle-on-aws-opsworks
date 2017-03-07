9ffb3bf50280dfdf49ad48c5d986b86b528e0d6f
Nick Mika <nicholas.mika0@gmail.com>
2017-03-07 15:19:01 +1100

    Include ssl.conf

--- cookbooks/moodle_opsworks/recipes/phpmyadminconfig.rb
+++ cookbooks/moodle_opsworks/recipes/phpmyadminconfig.rb
@@ -46,6 +46,15 @@ template 'ssl.conf' do
   mode '0644'
 end
 
+# nearly default config, added ssl.conf file
+template 'httpd.conf' do
+  path '/etc/httpd/conf/httpd.conf'
+  source 'httpd.conf'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
 template "/var/www/phpmyadmin/config.inc.php" do
 	source 'config.inc.php.erb'
 	owner user
