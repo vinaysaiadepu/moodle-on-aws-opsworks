6c4c292fc5a719819c1186030dd2408b4c14b65f
Nickmika <nicholas.mika0@gmail.com>
2017-09-25 13:02:02 +1000

    Change storage class to IA

--- cookbooks/moodle_opsworks/templates/backup-moodledata.sh
+++ cookbooks/moodle_opsworks/templates/backup-moodledata.sh
@@ -4,7 +4,7 @@ SIZE=$(du -sm /mnt/nfs/moodledata/filedir | awk '{print $1;}')
 if [ $SIZE -gt <%= @moodledata_size %> ]; then
   echo "Running backup for the $1 moodledata/filedir directory on <%= @stack %>";
   echo "Backup Started at $(date)" > /mnt/nfs/moodledata/filedir/start
-  aws s3 sync /mnt/nfs/moodledata/filedir s3://<%= @backup_bucket %>/<%= @stack %>/$1/moodledata/filedir --delete;
+  aws s3 sync /mnt/nfs/moodledata/filedir s3://<%= @backup_bucket %>/<%= @stack %>/$1/moodledata/filedir --delete --storage-class STANDARD_IA;
   echo "Completed Backup for the $1 moodledata/filedir directory on <%= @stack %>";
   echo "Backup Started at $(date)" > /mnt/nfs/moodledata/filedir/end
 else
