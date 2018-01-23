7dafc88077b28919bb6c505cced05d71deffe323
Nick Mika <nicholas.mika0@gmail.com>
2018-01-23 15:06:20 +1100

    improved backup scripts for monitoring purposes

--- cookbooks/moodle_opsworks/templates/backup-moodle-mysql.sh
+++ cookbooks/moodle_opsworks/templates/backup-moodle-mysql.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 <% @moodle_databases.each do |db| %>
-    /bin/nice -n 15 mysqldump -u <%= db[:db_user] %> -p<%= db[:db_pass] %> -h <%= db[:db_host] %> --single-transaction <%= db[:db_name] %> > /mnt/nfs/<%= db[:db_name] %>.sql
-    if test `find "/mnt/nfs/<%= db[:db_name] %>.sql" -mmin -60`
-    then
-        /bin/nice -n 15 tar -zcf /mnt/nfs/<%= db[:db_name] %>.tgz /mnt/nfs/<%= db[:db_name] %>.sql
-        /bin/nice -n 15 aws s3 cp /mnt/nfs/<%= db[:db_name] %>.tgz s3://<%= db[:backup_bucket] %>/<%= db[:stack] %>/$1/<%= db[:db_name] %>.tgz --storage-class STANDARD_IA
-        echo "Compress & Upload finished for <%= db[:db_name] %> $1"
-    else
-        echo "Mysql Dump for <%= db[:db_name] %> has likely failed as <%= db[:db_name] %>.sql is older than 60 minutes"
-    fi
-    
+	echo "$(date)" > /mnt/nfs/dbstart
+	/bin/nice -n 15 mysqldump -u <%= db[:db_user] %> -p<%= db[:db_pass] %> -h <%= db[:db_host] %> --single-transaction <%= db[:db_name] %> > /mnt/nfs/<%= db[:db_name] %>.sql
+	if test `find "/mnt/nfs/<%= db[:db_name] %>.sql" -mmin -60`
+	then
+		/bin/nice -n 15 tar -zcf /mnt/nfs/<%= db[:db_name] %>.tgz /mnt/nfs/<%= db[:db_name] %>.sql
+		if [ $? -eq 0 ]; then
+			echo "Compress finished for <%= db[:db_name] %> $1"
+			/bin/nice -n 15 aws s3 cp /mnt/nfs/<%= db[:db_name] %>.tgz s3://<%= db[:backup_bucket] %>/<%= db[:stack] %>/$1/<%= db[:db_name] %>.tgz --storage-class STANDARD_IA
+			if [ $? -eq 0 ]; then
+				echo "Upload finished for <%= db[:db_name] %> $1"
+				echo "$(date)" > /mnt/nfs/dbend
+				aws s3 cp /mnt/nfs/dbend s3://<%= @backup_bucket %>/<%= @stack %>/$1/dbend
+			else
+				echo "Upload failed for <%= db[:db_name] %> $1"
+			fi
+		else
+			echo "Compress failed for <%= db[:db_name] %> $1"
+		fi
+	else
+		echo "Mysql Dump for <%= db[:db_name] %> has likely failed as <%= db[:db_name] %>.sql is older than 60 minutes"
+	fi
 <%end %>
--- cookbooks/moodle_opsworks/templates/backup-moodledata.sh
+++ cookbooks/moodle_opsworks/templates/backup-moodledata.sh
@@ -2,11 +2,17 @@
 
 SIZE=$(du -sm /mnt/nfs/moodledata/filedir | awk '{print $1;}')
 if [ $SIZE -gt <%= @moodledata_size %> ]; then
-  echo "Running backup for the $1 moodledata/filedir directory on <%= @stack %>";
-  echo "Backup Started at $(date)" > /mnt/nfs/moodledata/filedir/start
-  aws s3 sync /mnt/nfs/moodledata/filedir s3://<%= @backup_bucket %>/<%= @stack %>/$1/moodledata/filedir --delete --storage-class STANDARD_IA;
-  echo "Completed Backup for the $1 moodledata/filedir directory on <%= @stack %>";
-  echo "Backup Started at $(date)" > /mnt/nfs/moodledata/filedir/end
+	echo "Running backup for the $1 moodledata/filedir directory on <%= @stack %>";
+	echo "$(date)" > /mnt/nfs/moodledata/filedir/start
+	/bin/nice -n 15 aws s3 sync /mnt/nfs/moodledata/filedir s3://<%= @backup_bucket %>/<%= @stack %>/$1/moodledata/filedir --delete --storage-class STANDARD_IA;
+	if [ $? -eq 0 ]; then
+		echo "Completed Backup for the $1 moodledata/filedir directory on <%= @stack %>";
+		echo "$(date)" > /mnt/nfs/moodledata/filedir/end
+		aws s3 cp /mnt/nfs/moodledata/filedir/end s3://<%= @backup_bucket %>/<%= @stack %>/$1/moodledata/filedir/end
+	else
+		echo "Sync command did not complete successfully for the $1 moodledata/filedir directory on <%= @stack %>";
+	fi
+
 else
-  echo "Files in  moodledata/filedir for the <%= @stack %> are smaller than expected please check directory, backup has not run";
+	echo "Files in  moodledata/filedir for the <%= @stack %> are smaller than expected please check directory, backup has not run";
 fi
